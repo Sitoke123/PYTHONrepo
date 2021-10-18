1805d944680908bb75fb26291567b52327a67eb2
Pranay_619 <83240428+Sitoke123@users.noreply.github.com>
2021-10-17 23:50:15 -0700

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,18 @@
+my_data = { 1:'pranay',2:'ram'}
+my_data[2]
+my_data.get(1)
+
+keys = ['pranay','ram']
+values = ['js','cs']
+data = dict(zip(keys,values)
+            
+prog = {'js':'atom','python':['pycharm','sublime']
+prog['js']
+prog['python']
+        
+# pop(key) , popitem(), clear() del
+sq= {1:1, 2:4, 3:9, 4:16, 5:25}
+print(sq)
+print(sq.pop(4))
+print(sq.popitem())
+print(sq.clear)        
