16b3d3aecfa87554ba06d82286934d11173082a3
Pranay_619 <83240428+Sitoke123@users.noreply.github.com>
2021-10-19 02:28:50 -0700

    Update object_class.py

--- object_class.py
+++ object_class.py
@@ -8,3 +8,12 @@ com2 = Computer()
 
 com1.config()
 com2.config()
+
+class Me:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+p1 = Me("Pranay", 20)
+p1.myfunc()
