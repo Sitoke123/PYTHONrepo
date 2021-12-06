24406d61f5aa33d0dd4d16182bb2065c08bbbe0b
Pranay_619 <83240428+Sitoke123@users.noreply.github.com>
2021-12-06 03:37:57 -0800

    Update Lambda.py

--- Lambda.py
+++ Lambda.py
@@ -9,3 +9,30 @@ f = lambda a : a*a
 result = f{4}
 
 print(result)
+
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "Lambda.ipynb",
+      "provenance": []
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "3idEPRedJ335",
+        "outputId": "ae966847-1c87-43b5-c00f-c45171a9fce0"
+      },
